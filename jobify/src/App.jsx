14e4298755bcdae5b12e7da75ea77a0cf74ebabd
Yash Manav <yashmanav2005@gmail.com>
2025-06-12 10:22:42 +0530

    day 2

---
+++ jobify/src/App.jsx
@@ -0,0 +1,12 @@
+import Login  from './Login';
+import Register from './Register';
+function App() {
+  return (
+    <div>
+        <h1>Root component</h1>
+        <Login />
+        <Register />
+    </div>
+  );
+}
+export default App;
---
+++ jobify/src/Login.jsx
@@ -0,0 +1,11 @@
+
+function Login() {
+    return (
+        <div>
+            <h1>This is login</h1>
+            <p>Enter user id and password</p>
+        </div>
+    )
+}
+
+export default Login;
