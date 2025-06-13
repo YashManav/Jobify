f25c1f60990369568a914fdc556c21ae280479bb
Yash Manav <yashmanav2005@gmail.com>
2025-06-13 10:28:51 +0530

    Routing and Global Css Done

--- jobify/src/App.jsx
+++ jobify/src/App.jsx
@@ -1,12 +1,25 @@
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Login  from './Login';
 import Register from './Register';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>HomePage</h1>
+  },
+  {
+    path: '/register',
+    element: <Register />
+  },
+  {
+    path: '/login',
+    element: <Login />  
+  }
+]);
+
 function App() {
   return (
-    <div>
-        <h1>Root component</h1>
-        <Login />
-        <Register />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 export default App;
