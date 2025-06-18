e03d346c80e516b9e78d17018cf6a5a15da3db32
Yash Manav <yashmanav2005@gmail.com>
2025-06-18 09:08:29 +0530

    Code Debugged till now

--- jobify/src/pages/index.js
+++ jobify/src/pages/index.js
@@ -3,6 +3,7 @@ export { default as Register } from './Register';
 export { default as Error } from './Error';
 export { default as Profile } from './Profile';
 export { default as Stats } from './Stats';
+export { default as HomeLayout } from './HomeLayout';
 export { default as AllJobs } from './AllJobs';
 export { default as AddJob } from './AddJob';
 export { default as Admin } from './Admin';
