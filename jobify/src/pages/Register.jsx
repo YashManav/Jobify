825214e2a07cd379745222321e8fc4d15db9a932
Yash Manav <yashmanav2005@gmail.com>
2025-06-19 10:46:13 +0530

    Register Page Done

--- jobify/src/pages/Register.jsx
+++ jobify/src/pages/Register.jsx
@@ -1,13 +1,31 @@
-import {Link } from 'react-router-dom';
+import FormRow from '../components/FormRow';
+import Logo from '../components/Logo';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Link } from 'react-router-dom';
 function Register(){
     return (
-        <div>
-            <h1>Register</h1>
-            <p>
-                This is the register page.
-            </p>
-            <Link to="/login">Login</Link>
-        </div>
-    )
-}
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='text' name='name' />
+        <FormRow type='text' name='lastName' labelText='last name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+
+        <FormRow type='password' name='password' />
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
 export default Register;
